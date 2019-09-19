98bf5d9cb97ef90acf25b16698ff31d048aba823
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2019-09-19 14:00:40 -0300

    Implementações iniciais do Ajax, troca de dados entre o servidor e o cliente

--- core/jogo/views.py
+++ core/jogo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def index(request):
@@ -9,3 +9,13 @@ def index(request):
 # Esta função irá receber o post salvando as variáveis no BD
 def novo_evento_batalha(request):
     pass
+
+
+@csrf_protect
+def teste(request):
+    if request.method == 'POST':
+        dano_total = request.POST['dano_total']
+
+        return render(request, 'jogo/teste.html', {'dano_total': dano_total})
+    else:
+        return 'Mano não deu certo ó'
