e7284351bb3f73fe4f6898ea4175555196678395
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2019-10-26 15:29:27 -0300

    Novo banco

--- core/core/settings.py
+++ core/core/settings.py
@@ -79,10 +79,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        # 'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        # 'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        # 'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
